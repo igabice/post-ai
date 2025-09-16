0221c23e867857f5c44e0afb4ed7ae08369124e6
Israel Gabice <igabice@gmail.com>
2025-09-16 19:10:48 +0000

    Try fixing this error: `Runtime Error: Error: useFieldArray is not defin

--- src/components/generate-plan/generate-plan-form.tsx
+++ src/components/generate-plan/generate-plan-form.tsx
@@ -1,6 +1,7 @@
+
 'use client';
 
-import { useForm } from 'react-hook-form';
+import { useForm, useFieldArray } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
 import { addDays, format } from 'date-fns';
@@ -88,7 +89,7 @@ export function GeneratePlanForm() {
     },
   });
 
-  const { fields, control } = useFieldArray({
+  const { fields } = useFieldArray({
     control: form.control,
     name: 'schedule',
   });
